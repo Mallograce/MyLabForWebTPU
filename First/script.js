e0a5cc122f1844fc5b2d7bbb109eedc6ef76bde6
Mallograce <mallocprof@gmail.com>
2023-10-12 16:40:18 +0700

    add droplist

--- First/script.js
+++ First/script.js
@@ -1,23 +1,21 @@
-const btn1 = document.getElementById('first_btn');
-const btn2 = document.getElementById('second_btn');
-const btn3 = document.getElementById('third_btn');
-const btn4 = document.getElementById('fourth_btn');
-const btn5 = document.getElementById('fifth_btn');
-const message = document.getElementById('message');
+const buttonContainer = document.getElementById('buttonContainer');
+const buttonCountSelect = document.getElementById('buttonCount');
 
-const buttons = [btn1, btn2, btn3, btn4, btn5];
-
-function checkButtons(button) {
-    for (let i of button) {
-        i.style.visibility = i.style.visibility === 'hidden' ? 'visible' : 'hidden';
-    }
-    if (buttons.every(button => button.style.visibility === 'hidden')) {
-        message.style.visibility = 'visible';
-    }
+function createButton(text) {
+    const button = document.createElement('button');
+    button.textContent = text;
+    button.classList.add('buttons_items');
+    return button;
 }
 
-btn1.addEventListener('click', () => checkButtons([btn3]));
-btn2.addEventListener('click', () => checkButtons([btn4]));
-btn3.addEventListener('click', () => checkButtons([btn5, btn1]));
-btn4.addEventListener('click', () => checkButtons([btn1]));
-btn5.addEventListener('click', () => checkButtons([btn2, btn5]));
+// Изменение при выбранном значении из droplist
+buttonCountSelect.addEventListener('change', () => {
+    buttonContainer.innerHTML = '';
+
+    const selectedCountButt = parseInt(buttonCountSelect.value);
+
+    for (let i = 1; i <= selectedCountButt; i++) {
+        const button = createButton(`Кнопка ${i}`);
+        buttonContainer.appendChild(button);
+    }
+})
